c890b40afa940dcde8a6a8788d34127379d34dc6
Clint Baxley <clint.baxley-ctr@ecstech.com>
2023-12-27 06:18:36 -0500

    Fix the wec server name setting

--- testing/configure/wec_gpo_update_server_name.ps1
+++ testing/configure/wec_gpo_update_server_name.ps1
@@ -1,5 +1,5 @@
 # To set the GP registry value
-Set-GPRegistryValue -Name "LME-WEC-Client" -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\EventForwarding\SubscriptionManager" -Value "Server=https://dc1.lme.local:5986/wsman/SubscriptionManager/WEC" -Type String
+Set-GPRegistryValue -Name "LME-WEC-Client" -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\EventForwarding\SubscriptionManager" -Value "Server=https://dc1.lme.local:5986/wsman/SubscriptionManager/WEC,Refresh=60" -Type String
 
 # To get the GP registry value to confirm it's set
 $registryValue = Get-GPRegistryValue -Name "LME-WEC-Client" -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\EventForwarding\SubscriptionManager"
