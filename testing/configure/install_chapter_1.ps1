b8db24a8066971763bf18043957a5be7ee7de548
Clint Baxley <clint.baxley-ctr@ecstech.com>
2023-12-28 07:24:23 -0500

    Adds the scripts to install chapter 1 and 2

--- testing/configure/install_chapter_1.ps1
+++ testing/configure/install_chapter_1.ps1
@@ -8,30 +8,17 @@ param (
 # Exit the script on any error
 $ErrorActionPreference = 'Stop'
 
-# Function to run a script and check for failure
-function RunScript {
-    param (
-        [string]$scriptPath
-    )
-
-    & $scriptPath
-    if ($LASTEXITCODE -ne 0 -or $?) {
-        Write-Error "Script failed: $scriptPath"
-        exit $LASTEXITCODE
-    }
-}
-
 # Change directory to the configure directory
 Set-Location -Path $configurePath
 
 # Run the scripts and check for failure
-RunScript '.\copy_files\create_lme_directory.ps1'
-RunScript '.\download_files.ps1 -directory lme'
-RunScript '.\wec_import_gpo.ps1 -directory lme'
-RunScript '.\wec_gpo_update_server_name.ps1'
-RunScript '.\create_ou.ps1'
-RunScript '.\wec_link_gpo.ps1'
-RunScript '.\wec_service_provisioner.ps1'
+.\copy_files\create_lme_directory.ps1
+.\download_files.ps1 -directory lme
+.\wec_import_gpo.ps1 -directory lme
+.\wec_gpo_update_server_name.ps1
+.\create_ou.ps1
+.\wec_link_gpo.ps1
+.\wec_service_provisioner.ps1
 
 # Run the wevtutil and wecutil commands
 wevtutil set-log ForwardedEvents /q:true /e:true
@@ -42,4 +29,4 @@ wecutil gr lme
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 # Run the move_computers_to_ou script
-RunScript '.\move_computers_to_ou.ps1'
+.\move_computers_to_ou.ps1
--- testing/configure/install_chapter_2.ps1
+++ testing/configure/install_chapter_2.ps1
@@ -8,24 +8,11 @@ param (
 # Exit the script on any error
 $ErrorActionPreference = 'Stop'
 
-# Function to run a script and check for failure
-function RunScript {
-    param (
-        [string]$scriptPath
-    )
-
-    & $scriptPath
-    if ($LASTEXITCODE -ne 0 -or $?) {
-        Write-Error "Script failed: $scriptPath"
-        exit $LASTEXITCODE
-    }
-}
-
 # Change directory to the configure directory
 Set-Location -Path $configurePath
 
 # Run the scripts and check for failure
-RunScript '.\sysmon_install_in_sysvol.ps1'
-RunScript '.\sysmon_import_gpo.ps1 -directory lme'
-RunScript '.\sysmon_gpo_update_vars.ps1'
-RunScript '.\sysmon_link_gpo.ps1'
+.\sysmon_install_in_sysvol.ps1
+.\sysmon_import_gpo.ps1 -directory lme
+.\sysmon_gpo_update_vars.ps1
+.\sysmon_link_gpo.ps1
