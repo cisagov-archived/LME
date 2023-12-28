348dd65a8720b8cbc62023bfe633a0d2212b219b
Clint Baxley <clint.baxley-ctr@ecstech.com>
2023-12-28 09:12:56 -0500

    Adds linux install scripts.

---
+++ testing/configure/linux_install_lme.sh
@@ -0,0 +1,6 @@
+# Download a copy of the LME files
+sudo git clone https://github.com/cisagov/lme.git /opt/lme/
+# Change to the LME directory containing files for the Linux server
+cd /opt/lme/Chapter\ 3\ Files/
+# Execute script with root privileges
+sudo ./deploy.sh install
